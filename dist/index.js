11faa2193aed0229b33af5ae5bc6e8be31704fee
longdhit <longdhit@gmail.com>
2018-06-11 13:13:52 +0700

    update

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("./startDb");
 require("./io/socket");
 const app_1 = require("./app");
-app_1.server.listen(process.env.PORT || 80, () => console.log(`Server started at ${app_1.server.address().port}`));
+process.setMaxListeners(0);
+app_1.server.listen(process.env.PORT || 3000, () => console.log(`Server started at ${app_1.server.address().port}`));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import './startDb';
 import './io/socket'
 import { server, app } from './app';
-server.listen(process.env.PORT || 80, () => console.log(`Server started at ${server.address().port}`))
+process.setMaxListeners(0);
+server.listen(process.env.PORT || 3000, () => console.log(`Server started at ${server.address().port}`))
