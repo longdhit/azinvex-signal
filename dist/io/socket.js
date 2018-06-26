1e9cf2735876bd3b4f90eee928209649ba751dc7
longdhit <longdhit@gmail.com>
2018-06-26 09:37:39 +0700

    update

--- dist/io/socket.js
+++ dist/io/socket.js
@@ -3,4 +3,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const socketIo = require("socket.io");
 const app_1 = require("../app");
 exports.io = socketIo(app_1.server);
-process.setMaxListeners(0);
--- src/io/socket.ts
+++ src/io/socket.ts
@@ -1,4 +1,3 @@
 import * as socketIo from 'socket.io';
 import { server } from '../app';
 export const io = socketIo(server);
-process.setMaxListeners(0);
